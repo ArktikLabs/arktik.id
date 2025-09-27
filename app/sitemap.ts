3848b6a90e21c8fb73da4ab8921e5850c3de4668
Khaer Ansori <khaer.ansori@traveloka.com>
2025-09-27 12:01:16 +0700

    feat: change sitemap baseUrl to www.arktik.id

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export const dynamic = 'force-static'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://arktik.id'
+  const baseUrl = "https://www.arktik.id";
   const currentDate = new Date().toISOString()
 
   return [
